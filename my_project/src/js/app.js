9bd2a4480d6e52f29bf5cc200a98951c60340e8b
yarik <yarik2015@protonmail.com>
2020-02-13 19:37:26 +0200

    my project

--- my_project/src/js/app.js
+++ my_project/src/js/app.js
@@ -10,7 +10,6 @@ hamburger.addEventListener('click', () => {
 		});
 });
 
-
 //scrool
 $(window).on("scroll", function(){
    if($(window).scrollTop()){
@@ -44,7 +43,7 @@ document.body.onload = function(){
 		if(!preloader.classList.contains('done')){
 			preloader.classList.add('done');
 		}
-	},2000);
+	},100);
 }
 
 //product
@@ -132,7 +131,7 @@ fetch('https://jsonplaceholder.typicode.com/users')
     {
         output += `
         <li>
-            ${user.name}
+           ${user.name}
         </li>
         `;
     });
@@ -140,6 +139,45 @@ fetch('https://jsonplaceholder.typicode.com/users')
     document.getElementById("response").innerHTML = output;
   });
 }
+
+//modal
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlayModal')
+
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
+  })
+})
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active')
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+})
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal')
+    closeModal(modal)
+  })
+})
+
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
+}
+
 
 
 //buttot top
@@ -149,7 +187,7 @@ $(document).ready(function() {
   let btn = $('#button');
 
   $(window).scroll(function() {
-    if ($(window).scrollTop() > 300) {
+    if ($(window).scrollTop() > 1400) {
       btn.addClass('show');
     } else {
       btn.removeClass('show');
@@ -161,3 +199,116 @@ $(document).ready(function() {
     $('html, body').animate({scrollTop:0}, '300');
   });
 });
+
+
+//add to cart
+
+if(document.readyState == 'loading') {
+  document.addEventListener('DOMContentLoaded', ready)
+} else {
+   ready()
+}
+
+function ready(){
+  let removeCart = document.getElementsByClassName('btn-danger');
+  for(let i = 0;i < removeCart.length; i++){
+    let button = removeCart[i]
+    button.addEventListener('click', removeCartItem)
+  }
+  let quantityInputs = document.getElementsByClassName('cart-quantity-input')
+  for(let i = 0; i < quantityInputs.length; i++){
+    let input = quantityInputs[i]
+    input.addEventListener('change', quantityChanged)
+  }
+  let addToCartButtons = document.getElementsByClassName('shop-item-button')
+  for(let i = 0; i < addToCartButtons.length; i++){
+    let button = addToCartButtons[i];
+    button.addEventListener('click', addToCartClicked)
+  }
+
+  document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked)
+
+  }
+
+function purchaseClicked(){
+  alert('Thank you for your purchase')
+ let cartItems = document.getElementsByClassName('cart-items')[0]
+ while(cartItems.hasChildNodes()){
+  cartItems.removeChild(cartItems.firstChild)
+ }
+ updateCartTotal()
+}
+
+function quantityChanged(event){
+  let input = event.target
+  if(isNaN(input.value) || input.value <= 0){
+      input.value =1
+    }
+    updateCartTotal()
+}
+
+
+function removeCartItem(event){
+  let buttonClicked = event.target
+    buttonClicked.parentElement.parentElement.remove()
+     updateCartTotal()
+}
+
+function addToCartClicked(event){
+  let button = event.target
+  let shopItem = button.parentElement.parentElement
+ let price = shopItem.getElementsByClassName('cart-price')[0].innerText
+ let imgCard = shopItem.getElementsByClassName('shop-img')[0].src
+ addItemToCard(price,imgCard)
+ updateCartTotal()
+}
+
+function addItemToCard(price,imgCard){
+  let cartRow = document.createElement('div')
+  cartRow.classList.add('cart-row')
+  let cartItems = document.getElementsByClassName ('cart-items')[0]
+  let cartItemNames = cartItems.getElementsByClassName('cart-price')
+  for (let i = 0; i < cartItemNames.length; i++){
+    if(cartItemNames[i].innerText == price){
+      alert('This item is already added to the cart')
+      return
+    }
+  }
+  let cartRowContents = `
+  <div class="cart-item cart-column">
+    <img class="cart-item-image " src="${imgCard}">
+  </div>
+  <span class="cart-price cart-column">${price}</span>
+  <form class="cart-quantity cart-column">
+    <input class="cart-quantity-input" type="number" value="1">
+    <button class="btn btn-danger" type="button">-</button>
+  </form>
+  `
+  cartRow.innerHTML = cartRowContents
+  cartItems.append(cartRow)
+  cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click', removeCartItem)
+  cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', quantityChanged)
+}
+
+
+
+
+function updateCartTotal() {
+  let cartItemContainer = document.getElementsByClassName('cart-items')[0]
+  let cartRows = cartItemContainer.getElementsByClassName('cart-row')
+  let total = 0
+  for(let i = 0; i < cartRows.length; i++) {
+    let cartRow = cartRows[i]
+    let priceElement = cartRow.getElementsByClassName('cart-price')[0]
+    let quantityElement = cartRow.getElementsByClassName('cart-quantity-input')
+    [0]
+    let price = parseFloat(priceElement.innerText.replace('$',''))
+    let quantity = quantityElement.value
+    total = total + (price * quantity)
+
+  }
+  total = Math.round(total * 100) / 100
+  document.getElementsByClassName('cart-total-price')[0].innerText = `$ ${total}`
+}
+
+
